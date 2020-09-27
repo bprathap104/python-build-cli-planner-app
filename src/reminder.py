0575200a679018260935c5b25d3ea6a425ea5cac
Saravanan <bprathap104@gmail.com>
2020-09-27 06:11:08 +0000

    inital commit

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,12 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    """This class acts is derived from base class PrefixedReminder
+    """
+    def __init__(self, text):
+        super().__init__()
+	self.prefix = "Please "
+        self.text   = text
+	return(self.prefix + self.test)
